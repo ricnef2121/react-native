6013012c117be9c9e6a537e87ed768bb1c4726fb
ricnef2121 <ricnef2121@gmail.com>
2020-01-28 08:28:37 -0600

    se agregan settimeot

--- AwesomeProject/src/components/Home/HomeView.js
+++ AwesomeProject/src/components/Home/HomeView.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
-import HomeViewStyles from './HomeViewStyles'
+import HomeViewStyles from './HomeViewStyles';
+import i18n from '../../i18n/i18n';
+
 const HomeView = () => {
+
+    const [time,setTime] = useState(0);
+
+    const startTime  = ()=>{
+        setInterval(() => {
+            setTime(time + 1000)
+        },1000);
+    }
+
+
+
+    const RenderRunningTimer = ()=>{
+        return(
+            <TouchableOpacity style={HomeViewStyles.mainActionButton} onPress={startTime}>
+                <Text style={HomeViewStyles.mainActionButtonText}>00:00:00</Text>
+            </TouchableOpacity>
+        );
+    }
+
+    const RenderStart = ()=>{
+        return(
+            <TouchableOpacity style={HomeViewStyles.mainActionButton} onPress={startTime}>
+            <Text style={HomeViewStyles.mainActionButtonText}>{i18n.HOME.START}</Text>
+        </TouchableOpacity>
+        );
+    }
+
   return (
-    <View style={{flex: 1,},HomeViewStyles.HomeViewContainer}>
+    <View style={{flex: 1},HomeViewStyles.HomeViewContainer}>
       <View stye={{flex: 1}}>
-        <Text style={HomeViewStyles.WelcomeHeader}>Good Mornig</Text>
+        <Text style={HomeViewStyles.WelcomeHeader}>{i18n.HOME.WELCOME_HEADER}</Text>
       </View>
       <View style={{flex: 2}}>
-        <TouchableOpacity style={HomeViewStyles.mainActionButton} onPress={() => console.log('button press')}>
-          <Text style={HomeViewStyles.mainActionButtonText}>START</Text>
-        </TouchableOpacity>
+         <Text> {time}</Text>
+        {RenderStart()}
+        {RenderRunningTimer()}
       </View>
     </View>
   );
